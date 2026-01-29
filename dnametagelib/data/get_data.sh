717defc523f52cfba46935acc0c4d6f3fcf2c921
oaxiom <oaxiom@gmail.com>
2026-01-29 17:45:07 +0800

    added some data download guidance

---
+++ dnametagelib/data/get_data.sh
@@ -0,0 +1,5 @@
+
+wget -c https://pyaging.s3.amazonaws.com/supporting_files/grimage2_subcomponents.csv
+wget -c https://pyaging.s3.amazonaws.com/supporting_files/grimage2.csv
+wget -c https://pyaging.s3.amazonaws.com/supporting_files/datMiniAnnotation3_Gold.csv
+
